1083531b2a960b82ce33549084de60085616cb52
nowilin1 <naimonpr1@gmail.com>
2022-10-27 10:56:36 +0800

    Попытка добавить изображение

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,28 +25,46 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
-            //MainFrame.Navigate(new Page1());
+            MainFrame.Navigate(new Materials());
             Manager.MainFrame = MainFrame;
 
             //Importsupplier();
 
         }
-        private void ButtonBack_Click(object sender, RoutedEventArgs e)
+        public void ImportSupplier()
         {
-            Manager.MainFrame.GoBack();
+
+            var fileData = File.ReadAllLines(@"\\FSProfile1.biik.ad.biik.ru\Redirect\shengeliya\Desktop\Вариант 1\Сессия 1\supplier_k_import.txt");
+            var images = Directory.GetFiles(@"\\FSProfile1.biik.ad.biik.ru\Redirect\shengeliya\Desktop\Вариант 1\Сессия 1\materials");
+            foreach (var line in fileData)
+            {
+                var data = line.Split('\t');
+                var tempTour = new
+                {
+                    Name = data[0].Replace("\"", ""),
+                    TicketsCount = int.Parse(data[2]),
+                    Price = decimal.Parse(data[3]),
+                    IsActual = (data[4] == "0") ? false : true
+                };
+
+            }
         }
-        //private void MainFrame_ContentRendered(object sender, EventArgs e)
-        //{
-        //    if (MainFrame.CanGoBack)
-        //    {
-        //        ButtonBack.Visibility = Visibility.Visible;
-        //    }
-        //    else
-        //    {
-        //        ButtonBack.Visibility = Visibility.Hidden;
-        //    }
-        //}
+            private void ButtonBack_Click(object sender, RoutedEventArgs e)
+            {
+                Manager.MainFrame.GoBack();
+            }
+            //private void MainFrame_ContentRendered(object sender, EventArgs e)
+            //{
+            //    if (MainFrame.CanGoBack)
+            //    {
+            //        ButtonBack.Visibility = Visibility.Visible;
+            //    }
+            //    else
+            //    {
+            //        ButtonBack.Visibility = Visibility.Hidden;
+            //    }
+            //}
 
 
+        }
     }
-}
--- WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,6 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("%d0%91%d0%be%d0%bb%d1%8c%d1%88%d0%b0%d1%8f%20%d0%bf%d0%b0%d1%87%d0%ba%d0%b0.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bigbase.context.tt")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bigbase.tt")]
 
 
